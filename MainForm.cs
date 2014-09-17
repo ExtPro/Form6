db8b15e2ed3f2b67f05db50b62acfdadb9eef8ea
ExtPro <maxsbox@mail.ru>
2014-09-17 18:40:32 +0300

    +(Form6) Добавлена главная форма и форма регистрации

---
+++ MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace Form6
+{
+    public partial class MainForm : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
